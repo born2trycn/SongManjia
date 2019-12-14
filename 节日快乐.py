c0f645211e98199827a5417fa0f5e04b05f20d0a
born2trycn <58068920+born2trycn@users.noreply.github.com>
2019-12-14 12:09:39 +0800

    Update 节日快乐.py

--- 节日快乐.py
+++ 节日快乐.py
@@ -1,20 +1,25 @@
 import time
-
+import turtle 
+lala=turtle.Pen()
+lala.forward(100)
 now = time.gmtime()
 print(now)
 
-month = now.tm_mon
 day = now.tm_mday
+month = now.tm_mon
 print(month)
 print(day)
+
+month = 3
+day = 15
 
 if (month == 3):
     if (day == 15):
         print('生日快乐')
+        lala.write('生日快乐')
 if (month == 7):
     if (day == 27):
         print('妈妈，生日快乐')
-if (month == 8):
     if (day == 30):
         print('爸爸，生日快乐')
 if (month == 6):
@@ -26,3 +31,4 @@ if (month == 5):
 if (month == 12):
     if (day == 7):
         print('大雪')
+turtle.done()
